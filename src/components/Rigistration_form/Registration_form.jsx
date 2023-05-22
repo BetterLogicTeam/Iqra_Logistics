cd1a369663461b65dda4ba497c1c659928276b6e
Haider <haideralirafeeq@gmail.com>
2023-05-22 18:32:43 +0500

    All form done

---
+++ src/components/Rigistration_form/Registration_form.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Button, Checkbox, Form, Input, Select } from "antd";
+const { Option } = Select;
+
+function Rigistration_form() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  const [form] = Form.useForm();
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select
+        style={{
+          width: 70,
+        }}
+      >
+        <Option value="92">+92</Option>
+        <Option value="91">+91</Option>
+      </Select>
+    </Form.Item>
+  );
+
+  return (
+    <div>
+      <div className="container overflow-hidden">
+        <div className="row justify-content-center py-5">
+          <div className="col-lg-6 border rounded-3 bg-light">
+            <h2 className="text-danger py-4">Registration form</h2>
+            <div className="email_frm px-3 px-md-5 pb-3">
+              <Form
+                name="basic"
+                initialValues={{
+                  remember: true,
+                }}
+                layout="vertical"
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+                className="mt-3"
+              >
+                <Form.Item
+                  label="Email"
+                  name="Email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Email!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Form.Item
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                  name="phone"
+                  label="Phone Number"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your phone number!",
+                    },
+                  ]}
+                >
+                  <Input
+                    addonBefore={prefixSelector}
+                    style={{
+                      width: "100%",
+                    }}
+                  />
+                </Form.Item>
+
+                <Form.Item
+                  label="Company Name"
+                  name="Company name"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your company name!",
+                    },
+                  ]}
+                >
+                  <Input />
+                </Form.Item>
+
+                <Button type="primary" htmlType="submit">
+                  Submit
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Rigistration_form;
